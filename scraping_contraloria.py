3bdb8ca0dade2b176d39daeddf2717302ae75dc5
Jorge Ortiz <jorgeor@unicauca.edu.co>
2021-10-03 22:58:07 -0500

    Nothing

--- scraping_contraloria.py
+++ scraping_contraloria.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-#Este es un comentario, intento 2, intento 3, intento 4
+
 
 #driver = webdriver.Firefox()
 driver = webdriver.Chrome()
