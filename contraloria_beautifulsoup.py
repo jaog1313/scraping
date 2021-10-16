cf109b67e3207ee419300c2929bdd6f4ee60f409
Alexei Zharkov <social_2549@protonmail.com>
2021-10-16 16:59:19 -0500

    Empezando a hacerlo con BeautifulSoup cumpliendo los nuevos requerimientos

---
+++ contraloria_beautifulsoup.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+def scrap_url(url, id_number, type_id)
+    html = urlopen(url)
+    bsObj = BeutifulSoup(html)
+    namelist = bsObj.findAll()
+    for name in namelist:
+
+if __name__ == "__main__":
+    url = "https://www.contraloria.gov.co/control-fiscal/responsabilidad-fiscal/control-fiscal/responsabilidad-fiscal/certificado-de-antecedentes-fiscales/persona-natural"
+    id_number = 123456789
+    n = len(sys.argv)
+    if(n==1):
+        scrap_url(url, id_number, "1")
+    elif(n==2):
+        if(re.search(".csv", sys.argv[1])!=None):
+            print("Read the database")
+        else:
+            scrap_url(url, sys.argv[1], "1")
+    elif(n==3):
+            scrap_url(url, sys.argv[1], sys.argv[2])
